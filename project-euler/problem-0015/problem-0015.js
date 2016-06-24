cce3eb31582f8a207b25944d9fac21bf5ccebf47
gpawlik <pawlik.gregor@gmail.com>
2016-06-23 19:03:10 -0700

    Project Euler 15 updates to handle large numbers

--- project-euler/problem-0015/problem-0015.js
+++ project-euler/problem-0015/problem-0015.js
@@ -1,14 +1,37 @@
 'use strict';
-const factorial = require('../../common/math/factorial').factorial;
+const BigNumber = require('bignumber.js');
+BigNumber.config({ EXPONENTIAL_AT: 1e+9 });
+BigNumber.config({ POW_PRECISION: 1e+9 });
 
 /**
  * Calculates the count of the number of possible lattice paths through a grid
  * Uses the factorial formula for the n + m take n binomial coefficient: (n + m)! / (n! x m!)
+ * Common factorials cancelled out in formula to reduce the potentially large numbers being multiplied
  * For a n x n square, could result in formula: (2n)! / (n! x n!)
  * @param {Number} n
  * @param {Number} m
+ * @param {Number?} modulo
  * @returns {Number}
  */
-exports.latticePaths = (n, m) => {
-  return factorial(n + m) / factorial(n) / factorial(m);
+exports.latticePaths = (n, m, modulo = null) => {
+  // get an array of numbers for 1 to n+m
+  let numerator = [...Array(n + m + 1).keys()].slice(1);
+
+  // get an array of numbers of 1 to n, and 1 to m
+  let denominator = [...Array(n + 1).keys()].slice(1).concat([...Array(m + 1).keys()].slice(1));
+
+  // cancel out any numbers that are in both numerator and denominator
+  let i = denominator.length;
+  while (i--) {
+    let index = numerator.indexOf(denominator[i]);
+    if (index >= 0) {
+      numerator.splice(index, 1);
+      denominator.splice(i, 1);
+    }
+  }
+
+  // number of items in denominator will equal number in numerator so loop and multiply and divide
+  let result = numerator.reduce((previous, current, index) => previous.times(current).dividedBy(denominator[index]), new BigNumber(1));
+  result = modulo ? result.modulo(modulo) : result;
+  return result.toNumber();
 };
--- project-euler/problem-0015/problem-0015.spec.js
+++ project-euler/problem-0015/problem-0015.spec.js
@@ -4,5 +4,11 @@ const problem15 = require('./problem-0015');
 describe('latticePaths', () => {
   it('calculates paths for sample input', () => {
     expect(problem15.latticePaths(2,2)).toBe(6);
+    expect(problem15.latticePaths(3,2)).toBe(10);
+  });
+
+  it('calculates paths for hacker rank modulo results', () => {
+    expect(problem15.latticePaths(500, 500, 1000000007)).toBe(159835829);
+    expect(problem15.latticePaths(3, 4) % 1000000007).toBe(35);
   });
 });
