a62f16c58833d6e3a0b1bd15dbf4a56776f5a31c
gpawlik <pawlik.gregor@gmail.com>
2016-07-23 21:13:42 -0700

    Project Euler 29

--- project-euler/problem-0029/problem-0029.spec.js
+++ project-euler/problem-0029/problem-0029.spec.js
@@ -4,5 +4,8 @@ const problem29 = require('./problem-0029');
 describe('distinctPowers', () => {
   it('counts distinct powers for known input', () => {
     expect(problem29.distinctPowers(5)).toEqual(15);
+    expect(problem29.distinctPowers(100)).toEqual(9183);
+    expect(problem29.distinctPowers(256)).toEqual(62136);
+    expect(problem29.distinctPowers(1000)).toEqual(977358);
   });
 });
